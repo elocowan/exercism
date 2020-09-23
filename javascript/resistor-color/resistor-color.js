4b50c7912d988c601aa0c3e2c56b0e290df95416
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-23 07:56:37 -0600

    Solves resistor-color with COLORS object
    Doesn't match tests which use COLORS array

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,51 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+// create a way:
+// to look up the numerical value associated with a particular color band
+// to list the different band colors
+
+// input: color
+// output: numerical value of that band
+// constraints
+// edge cases: capitalization
+
+// if no color is specified, return the keys of the Colors object so user can see what their choices are
+
+// define COLORS as an object full of color keys with associated values
+
+// PseudoCode
+// if COLORS[color] is truthy
+  // return COLORS[color]
+// return Object.keys(COLORS), (maybe with a message that these are the available colors)
+
+// samples:
+// colorCode("Green") --> 5
+// colorCode() --> array of colors
+// colorCode("Maroon") --> array of colors
+
+// export
+const colorCode = (color) => {
+  if (COLORS[color]) {
+    return COLORS[color];
+  }
+  return Object.keys(COLORS);
 };
 
-export const COLORS = undefined;
+// export
+const COLORS = {
+  Black: 0,
+  Brown: 1,
+  Red: 2,
+  Orange: 3,
+  Yellow: 4,
+  Green: 5,
+  Blue: 6,
+  Violet: 7,
+  Grey: 8,
+  White: 9
+};
+
+console.log(colorCode("Green"));
+console.log(colorCode());
+console.log(colorCode("Maroon"));
