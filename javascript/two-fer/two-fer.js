5133ba1c07d029a920dbf34fdfb625795b71a9f6
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-22 20:13:01 -0600

    Solves two-fer

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -3,6 +3,31 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = () => {
-  throw new Error("Remove this statement and implement this function");
+// i: name
+// o: a string with message -- "One for X, one for me."
+// c:
+// e: if name is missing, return "One for you, one for me", what if name is not a string?
+
+// samples:
+  // twoFer(Alice) --> "One for Alice, one for me."
+  // twoFer(Bob) --> "One for Bob, one for me."
+  // twoFer() --> "One for you, one for me."
+
+
+// psuedocode
+  // if name is undefined
+    // return "One for you, one for me."
+  // return `One for ${name}, one for me.`
+
+export const twoFer = (name) => {
+  if (!name) {
+    return "One for you, one for me."
+  }
+
+  if (typeof name === "string") {
+    return `One for ${name}, one for me.`
+  }
 };
+
+console.log(twoFer("Alice"));
+console.log(twoFer());
