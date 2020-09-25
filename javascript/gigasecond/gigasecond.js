8d533ff4ca9fc06465a960039db7a2f996f303a2
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-25 16:32:25 -0600

    Solve gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,13 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// i: Given a moment,
+// o: the moment that would be after a gigasecond has passed (10^9 seconds)
+// c: don't mutate function argument
+// e:
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = (date) => {
+  // getTime() returns the number of milliseconds since the UNIX epoch -- will work even if before UNIX epoch (returns negative number)
+  let startTime = date.getTime();
+  // add 10**12 milliseconds
+  let endTime = startTime + (10**12);
+  // make a new Date for that number of milliseconds relative to the UNIX epoch
+  return new Date(endTime);
 };
