323e370a71555b611a6def04a40da43ca3630e1b
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-25 10:01:37 -0600

    Solve resistor-color-duo using reduce

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,25 +1,11 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
 // will take color names as input and
 // output a two digit number, even if the input is more than two colors!
 
 export const decodedValue = (array) => {
-  let totalString = '';
+  let totalString = array.reduce((acc, cur) => {return acc + colors.indexOf(cur)}, '');
 
-  for (var i = 0; i < 2; i++) {
-    let currentElement = array[i];
-    let currentIndex = colors.indexOf(currentElement);
-    totalString += currentIndex.toString();
-  }
-
-  return parseInt(totalString);
+  let firstTwo = totalString.slice(0,2);
+  return parseInt(firstTwo);
 };
-
-console.log(decodedValue(['brown', 'green'])); // --> 15
-console.log(decodedValue(['brown', 'green', 'violet'])); // --> 15
-console.log(decodedValue(['brown', 'black'])); // --> 10
