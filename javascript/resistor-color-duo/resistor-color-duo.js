504d54f6a5febf5403e7f60bfc1d0f24dd11e7ef
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-25 09:40:50 -0600

    Solve resistor-color-duo using for-loop

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+
+// will take color names as input and
+// output a two digit number, even if the input is more than two colors!
+
+export const decodedValue = (array) => {
+  let totalString = '';
+
+  for (var i = 0; i < 2; i++) {
+    let currentElement = array[i];
+    let currentIndex = colors.indexOf(currentElement);
+    totalString += currentIndex.toString();
+  }
+
+  return parseInt(totalString);
 };
+
+console.log(decodedValue(['brown', 'green'])); // --> 15
+console.log(decodedValue(['brown', 'green', 'violet'])); // --> 15
+console.log(decodedValue(['brown', 'black'])); // --> 10
