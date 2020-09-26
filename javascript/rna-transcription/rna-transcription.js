25abbbf8879f960cc243f2cb3af8fbf1de7a41ca
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-26 12:34:14 -0600

    Solve rna-transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,8 +1,18 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// i: DNA strand
+// o: RNA complement
+// `G` -> `C`
+// `C` -> `G`
+// `T` -> `A`
+// `A` -> `U`
 
-export const toRna = () => {
-  throw new Error("Remove this statement and implement this function");
+export const translator = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U'
+}
+
+export const toRna = (dna) => {
+  let dnaArray = dna.split('');
+  return dnaArray.reduce((acc, cur) => acc + translator[cur], '');
 };
