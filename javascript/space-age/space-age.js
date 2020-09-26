07752b54263e882c0e9e6a45d2e818d79a73c7e6
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-26 13:30:38 -0600

    Solve space-age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,20 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// i: planet and age in seconds
+// o: age in years
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const orbital_periods = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1, // 365.25 Earth days, or 31,557,600 seconds
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
+
+export const age = (planet, seconds) => {
+  let ageInEarthYears = seconds / 31557600;
+
+  // toFixed method returns a string, so it's necessary to parse the float to return the type the tests are expecting
+  return parseFloat((ageInEarthYears / orbital_periods[planet]).toFixed(2));
 };
