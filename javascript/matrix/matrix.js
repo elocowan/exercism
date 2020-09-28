2b0844aa2180ffb9ba2a12f2af177d21ac20a937
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-28 09:00:57 -0600

    Solve matrix

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,18 +1,30 @@
-//
-// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Matrix {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(matrixString) {
+    this.matrixStrings = matrixString.split('\n');
+    this.matrixNumbers = this.matrixStrings.map((string) => {
+      let charRow = string.split(' ');
+      let numberRow = charRow.map((element) => parseInt(element));
+      return numberRow;
+    });
   }
 
   get rows() {
-    throw new Error("Remove this statement and implement this function");
+    return this.matrixNumbers;
   }
 
   get columns() {
-    throw new Error("Remove this statement and implement this function");
+    let columns = [];
+    this.matrixNumbers.forEach((row) => {
+      row.forEach((element, index) => {
+        if (!columns[index]) {
+          columns[index] = [];
+          columns[index].push(element);
+        } else {
+          columns[index].push(element);
+        }
+      })
+    })
+
+    return columns;
   }
 }
