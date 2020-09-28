0639aa0bc9d1c9f3eb4646cadffb6f19cb2331cf
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-28 08:13:52 -0600

    Solve pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,8 +1,38 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const isPangram = (sentence) => {
+  const alphabet = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    j: 0,
+    k: 0,
+    l: 0,
+    m: 0,
+    n: 0,
+    o: 0,
+    p: 0,
+    q: 0,
+    r: 0,
+    s: 0,
+    t: 0,
+    u: 0,
+    v: 0,
+    w: 0,
+    x: 0,
+    y: 0,
+    z: 0
+  }
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+  for (var i = 0; i < sentence.length; i++) {
+    let currentLowerCase = sentence[i].toLowerCase();
+    alphabet[currentLowerCase]++
+  }
+
+  let alphabetValues = Object.values(alphabet);
+  return !alphabetValues.includes(0);
 };
