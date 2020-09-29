ef299aa49744ed526631397bc5e17d64f80133fc
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-29 08:03:14 -0600

    Solve leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,8 +1,18 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// i: year
+// o: boolean (is leap year or is not leap year)
 
-export const isLeap = () => {
-  throw new Error("Remove this statement and implement this function");
+// on every year that is evenly divisible by 4 except every year that is evenly divisible by 100 unless the year is also evenly divisible by 400
+
+// 1997 is not a leap year, but 1996 is.  1900 is not a leap year, but 2000 is.
+
+export const isLeap = (year) => {
+  let isDivisibleBy4 = (year % 4 === 0);
+  let isDivisibleBy100 = (year % 100 === 0);
+  let isDivisibleBy400 = (year % 400 === 0);
+
+  if (isDivisibleBy4 && (!isDivisibleBy100 || isDivisibleBy400)) {
+    return true;
+  }
+
+  return false;
 };
