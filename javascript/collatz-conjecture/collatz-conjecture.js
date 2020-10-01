b71a9ffd6b9be256a7d05ba68fe2e4ba783004ef
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-30 20:06:12 -0600

    Solve collatz-conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,8 +1,33 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// i: any positive integer n
+// o: return the number of steps required to reach 1 using steps
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+// if n is even, divide n by 2 to get n / 2
+// if n is odd, multiply n by 3 and add 1 to get 3n + 1
+
+// sample: n = 12
+// 0. 12
+// 1. 6
+// 2. 3
+// 3. 10
+// 4. 5
+// 5. 16
+// 6. 8
+// 7. 4
+// 8. 2
+// 9. 1
+
+export const steps = (int) => {
+  if (int <= 0) {
+    throw "Only positive numbers are allowed";
+  }
+  let count = 0;
+  while (int !== 1) {
+    if (int % 2 === 0) {
+      int /= 2;
+    } else {
+      int = int * 3 + 1;
+    }
+    count++
+  }
+  return count;
 };
