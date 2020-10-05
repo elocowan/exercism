0dfde402491a603d68a9288eadae4636ec067cf9
Ethan Cowan <ethan.cowan@gmail.com>
2020-10-05 15:59:10 -0600

    Solve triangle

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,22 +1,45 @@
-//
-// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
   }
 
   isEquilateral() {
-    throw new Error("Remove this statement and implement this function");
+    if (this.side1 === 0 || this.side2 === 0 || this.side3 === 0) {
+      return false;
+    } else if (this.side1 === this.side2 && this.side1 === this.side3) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isIsosceles() {
-    throw new Error("Remove this statement and implement this function");
+    if ((this.side1 + this.side2) < this.side3) {
+      return false;
+    } else if ((this.side1 + this.side3) < this.side2) {
+      return false;
+    } else if ((this.side2 + this.side3) < this.side1) {
+      return false;
+    } else {
+      return (
+        this.side1 === this.side2 ||
+        this.side2 === this.side3 ||
+        this.side1 === this.side3
+      )
+    }
   }
 
   isScalene() {
-    throw new Error("Remove this statement and implement this function");
+    if ((this.side1 + this.side2) < this.side3) {
+      return false;
+    } else if ((this.side1 + this.side3) < this.side2) {
+      return false;
+    } else if ((this.side2 + this.side3) < this.side1) {
+      return false;
+    } else {
+      return (this.side1 !== this.side2 && this.side2 !== this.side3)
+    }
   }
 }
