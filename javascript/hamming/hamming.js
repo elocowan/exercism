92b2f972d9042d7c5c3ab0dcb9e6c5f142d3612d
Ethan Cowan <ethan.cowan@gmail.com>
2020-10-07 13:34:42 -0600

    Solve hamming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,8 +1,27 @@
-//
-// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+
+export const compute = (left, right) => {
+  if (!left.length && !right.length) {
+    return 0;
+  }
+
+  if (!left.length && right.length) {
+    throw 'left strand must not be empty'
+  }
+
+  if (!right.length && left.length) {
+    throw 'right strand must not be empty'
+  }
+
+  if (left.length !== right.length) {
+    throw 'left and right strands must be of equal length'
+  }
+
+  let hamming = 0;
+  for(let i = 0; i < left.length; i++) {
+    if (left[i] !== right[i]) {
+      hamming++;
+    }
+  }
+  return hamming;
 };
