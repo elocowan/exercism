f45d2f6d9db4326a9ec4138d1a2884e5a979d79c
Ethan Cowan <ethan.cowan@gmail.com>
2020-10-09 22:31:11 -0600

    Incomplete solution for word-count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,8 +1,14 @@
-//
-// This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const countWords = () => {
-  throw new Error("Remove this statement and implement this function");
+export const countWords = (string) => {
+  let punctuationless = string.replace(/[.\/#!$%@\^&\*;:{}=\-_`~()]/g,"");
+  let finalString = punctuationless.replace(/\s{2,}/g," ");
+  let wordArray = finalString.trim().toLowerCase().split(/[\s,]+/)
+  console.log(wordArray);
+  return wordArray.reduce((acc, word) => {
+    if (acc[word]) {
+      acc[word]++;
+    } else {
+      acc[word] = 1
+    }
+    return acc
+  }, {});
 };
