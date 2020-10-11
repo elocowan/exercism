84fa3f5eb458eef7bd01a018c38f7b695dc6e7f6
Ethan Cowan <ethan.cowan@gmail.com>
2020-10-10 19:22:22 -0600

    Solve word-count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,14 +1,13 @@
 export const countWords = (string) => {
-  let punctuationless = string.replace(/[.\/#!$%@\^&\*;:{}=\-_`~()]/g,"");
-  let finalString = punctuationless.replace(/\s{2,}/g," ");
-  let wordArray = finalString.trim().toLowerCase().split(/[\s,]+/)
-  console.log(wordArray);
-  return wordArray.reduce((acc, word) => {
-    if (acc[word]) {
-      acc[word]++;
-    } else {
-      acc[word] = 1
-    }
-    return acc
-  }, {});
+  return string
+    .toLowerCase()
+    .match(/([0-9]|[a-z]+('[a-z]+)?)/g)
+    .reduce((acc, word) => {
+      if (acc[word]) {
+        acc[word]++;
+      } else {
+        acc[word] = 1
+      }
+      return acc
+    }, {});
 };
